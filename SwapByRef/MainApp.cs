46c0fda6a697f28ddf61fa4dadc5602ee7c30b1d
Raian <leeranleeran30@gmail.com>
2023-03-21 15:38:50 +0900

    2303211538

---
+++ SwapByRef/MainApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapByRef
+{
+    class MainApp
+    {
+        static void Swap(ref int a, ref int b) //ref 매개변수는 주소값이 넘어간다.
+        {
+            int temp = b; //b의 값이 넘어간다.
+            b = a;
+            a = temp;
+        }
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 4;
+            Console.WriteLine($"x:{x}, y:{y}");
+
+            Swap(ref x, ref y);//ref 참조에 의한 전달
+
+            Console.WriteLine($"x:{x}, y:{y}");
+        }
+    }
+}
---
+++ SwapByValue/MainApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapByValue
+{
+    class MainApp
+    {
+        public static void swap(int a, int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 4;
+
+            Console.WriteLine($"X:{x}, y:{y}");
+
+            swap(x, y);
+
+            Console.WriteLine($"X:{x}, y:{y}"); // 매개변수는 데이터가 복사되는 것, 별개의 데이터 공간이다.
+        }
+    }
+}

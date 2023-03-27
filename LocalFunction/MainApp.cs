719f54061cb1380ef260e6b0c50119f988b5d10b
Raian <leeranleeran30@gmail.com>
2023-03-27 14:59:27 +0900

    2303271459

---
+++ LocalFunction/MainApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalFunction
+{
+    class MainApp
+    {
+        static string ToLowerString(string input)
+        {
+            var arr = input.ToCharArray();       //문자열을 char[] 문자배열로 변환 // var타입형: 컴파일러가 데이터 타입을 부여
+
+            for (int i = 0; i < arr.Length; i++) //char 배열 길이만큼 반복 
+            {
+                arr[i] = ToLowerChar(i);
+            }
+            char ToLowerChar(int i)             //반환 값이 char 캐릭터
+            {
+                if (arr[i] < 65 || arr[i] > 90) //A~Z의 ASCII 아스키코드 값 : 65~90 // 또는
+                    return arr[i];
+                else  // a~z의 ASCII값 :97~122
+                    return (char)(arr[i] + 32); //대문자를 소문자로 변환해주는 것 H -> h 
+            }
+            return new string(arr);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ToLowerString("Hello!"));
+            Console.WriteLine(ToLowerString("Good Morning."));
+            Console.WriteLine(ToLowerString("This is C#."));
+        }
+    }
+}
---
+++ OptionalParameter/MainApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameter
+{
+    class MainApp
+    {
+        static void PrintProfile(string name, string phone = "") //string phone = "" 매개변수에서 초기화
+        {
+            Console.WriteLine($"Name:{name}, Phone:{phone}");
+        }
+
+
+        static void Main(string[] args)
+        {
+            PrintProfile("중근");                  //선택적 인수 : 매개변수가 기본 값을 가지므로 인수를 생략 가능
+            PrintProfile("관순", "010-123-1234");
+            PrintProfile(name: "봉길");            //명명된 인수
+            PrintProfile(name: "동주", phone:"010-789-7890");
+        }
+    }
+}

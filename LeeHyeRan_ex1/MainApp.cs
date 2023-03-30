0b01d1fa38f87dc1f8b2663a7abb97551a26278a
Raian <leeranleeran30@gmail.com>
2023-03-30 15:44:13 +0900

    2303301544

---
+++ LeeHyeRan_ex1/MainApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeeHyeRan_ex1
+{
+    class MainApp
+    {
+        static double Square(double arg)
+        {
+            //메소드 구현
+            return arg * arg;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("수를 입력하세요 : ");
+            string input = Console.ReadLine();
+            double arg = Convert.ToDouble(input);
+
+            Console.WriteLine("결과 : {0}", Square(arg));
+        }
+    }
+}
